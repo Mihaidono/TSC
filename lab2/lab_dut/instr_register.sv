2acc4d03781ca7cbf3b753ea5ea8abea015f160a
Mihaidono <amihai2001@gmail.com>
2024-03-10 21:03:39 +0200

    tema 1

--- lab2/lab_dut/instr_register.sv
+++ lab2/lab_dut/instr_register.sv
@@ -7,39 +7,54 @@
  **********************************************************************/
 
 module instr_register
-import instr_register_pkg::*;  // user-defined types are defined in instr_register_pkg.sv
-(input  logic          clk,
- input  logic          load_en,
- input  logic          reset_n,
- input  operand_t      operand_a,
- input  operand_t      operand_b,
- input  opcode_t       opcode,
- input  address_t      write_pointer,
- input  address_t      read_pointer,
- output instruction_t  instruction_word
-); // daca asignam unei val de tip input o valoare -> eroare
-  timeunit 1ns/1ns;
+  import instr_register_pkg::*;  // user-defined types are defined in instr_register_pkg.sv
+(
+    input  logic         clk,
+    input  logic         load_en,
+    input  logic         reset_n,
+    input  operand_t     operand_a,
+    input  operand_t     operand_b,
+    input  opcode_t      opcode,
+    input  address_t     write_pointer,
+    input  address_t     read_pointer,
+    output instruction_t instruction_word
+);  // daca asignam unei val de tip input o valoare -> eroare
+  timeunit 1ns / 1ns;
 
-  instruction_t  iw_reg [0:31];  // an array of instruction_word structures
+  instruction_t iw_reg[0:31];  // an array of instruction_word structures
 
   // write to the register
-  always@(posedge clk, negedge reset_n)   // write into register
+  always @(posedge clk, negedge reset_n)  // write into register
     if (!reset_n) begin
-      foreach (iw_reg[i])
-        iw_reg[i] = '{opc:ZERO,default:0};  // reset to all zeros, default 0 -> reseteaza A si B, starea default la var de 4 stari -> X
+      foreach (iw_reg[i]) begin
+        iw_reg[i] = '{opc: ZERO, default: 0};  
+        // reset to all zeros, default 0 -> reseteaza A si B, starea default la var de 4 stari -> X
+      end
+    end 
+    else begin
+      if (load_en) begin
+        // iw_reg[write_pointer] = '{opcode,operand_a,operand_b};
+        case (opcode)
+          ZERO:  iw_reg[write_pointer] = '{opcode, operand_a, operand_b, {64{1'b0}}};
+          PASSA: iw_reg[write_pointer] = '{opcode, operand_a, operand_b, operand_a};
+          PASSB: iw_reg[write_pointer] = '{opcode, operand_a, operand_b, operand_b};
+          ADD:   iw_reg[write_pointer] = '{opcode, operand_a, operand_b, operand_a + operand_b};
+          SUB:   iw_reg[write_pointer] = '{opcode, operand_a, operand_b, operand_a - operand_b};
+          MULT:  iw_reg[write_pointer] = '{opcode, operand_a, operand_b, operand_a * operand_b};
+          DIV:   iw_reg[write_pointer] = '{opcode, operand_a, operand_b, operand_a / operand_b};
+          MOD:   iw_reg[write_pointer] = '{opcode, operand_a, operand_b, operand_a % operand_b};
+        endcase
+      end  // in caz de eroare sv truncheaza -> da overflow si taie ce da overflow
     end
-    else if (load_en) begin
-      iw_reg[write_pointer] = '{opcode,operand_a,operand_b};
-    end // in caz de eroare sv truncheaza -> da overflow si taie ce da overflow
 
   // read from the register
   assign instruction_word = iw_reg[read_pointer];  // continuously read from register
 
-// compile with +define+FORCE_LOAD_ERROR to inject a functional bug for verification to catch
+  // compile with +define+FORCE_LOAD_ERROR to inject a functional bug for verification to catch
 `ifdef FORCE_LOAD_ERROR
-initial begin
-  force operand_b = operand_a; // cause wrong value to be loaded into operand_b
-end
+  initial begin
+    force operand_b = operand_a;  // cause wrong value to be loaded into operand_b
+  end
 `endif
 
-endmodule: instr_register
+endmodule : instr_register
--- lab2/lab_dut/instr_register_pkg.sv
+++ lab2/lab_dut/instr_register_pkg.sv
@@ -20,10 +20,13 @@ package instr_register_pkg;
   
   typedef logic [4:0] address_t;
   
+  typedef logic signed [63:0] operand_result_t;
+
   typedef struct {
     opcode_t  opc;
     operand_t op_a;
     operand_t op_b;
+    operand_result_t result;
   } instruction_t;
 
 endpackage: instr_register_pkg
